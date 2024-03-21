210965eaa74f6b0ba93cbe7d8ab367918ca85f98
Clément Patout <2178406+clempat@users.noreply.github.com>
2024-03-21 22:59:02 +0100

    feat: bring back copilot completion on tab for now

--- lua/clement/plugins/copilot.lua
+++ lua/clement/plugins/copilot.lua
@@ -1,10 +1,15 @@
 return {
 	"zbirenbaum/copilot.lua",
+	build = ":Copilot auth",
 	config = function()
 		local copilot = require("copilot")
 		copilot.setup({
 			suggestion = {
+				enabled = true,
 				auto_trigger = true,
+				keymap = {
+					accept = "<Tab>",
+				},
 			},
 			-- Node.js version must be > 16.x
 			-- copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v18.18.0/bin/node", -- Node.js version must be > 16.x
