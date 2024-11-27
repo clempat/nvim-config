119b0aac1c1ff498298b0afcbc63d1f5a238a9c4
Clément Patout <2178406+clempat@users.noreply.github.com>
2024-11-27 12:17:23 +0100

    fix: update to new venvselector

--- lua/clement/plugins/python.lua
+++ lua/clement/plugins/python.lua
@@ -43,19 +43,19 @@ return {
 	},
 	{
 		"linux-cultist/venv-selector.nvim",
-		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-		cmd = "VenvSelect",
-		opts = function(_, opts)
-			opts.dap_enabled = true
-			return vim.tbl_deep_extend("force", opts, {
-				name = {
-					"venv",
-					".venv",
-					"env",
-					".env",
-				},
-			})
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"mfussenegger/nvim-dap",
+			"mfussenegger/nvim-dap-python", --optional
+			{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+		},
+		lazy = false,
+		branch = "regexp", -- This is the regexp branch, use this for the new version
+		config = function()
+			require("venv-selector").setup()
 		end,
-		keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+		keys = {
+			{ ",v", "<cmd>VenvSelect<cr>" },
+		},
 	},
 }
