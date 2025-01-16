007b4281dbd1ac06b32d8626e2ffc91da6c584ef
Clément Patout <2178406+clempat@users.noreply.github.com>
2025-01-16 10:44:35 +0100

    fix: tmp delete venv

--- lua/clement/plugins/python.lua
+++ lua/clement/plugins/python.lua
@@ -41,21 +41,23 @@ return {
 			end,
 		},
 	},
-	{
-		"linux-cultist/venv-selector.nvim",
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"mfussenegger/nvim-dap",
-			"mfussenegger/nvim-dap-python", --optional
-			{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-		},
-		lazy = false,
-		branch = "regexp", -- This is the regexp branch, use this for the new version
-		config = function()
-			require("venv-selector").setup()
-		end,
-		keys = {
-			{ ",v", "<cmd>VenvSelect<cr>" },
-		},
-	},
+	-- {
+	-- 	"linux-cultist/venv-selector.nvim",
+	-- 	dependencies = {
+	-- 		"neovim/nvim-lspconfig",
+	-- 		"mfussenegger/nvim-dap",
+	-- 		"mfussenegger/nvim-dap-python", --optional
+	-- 		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+	-- 	},
+	-- 	lazy = false,
+	-- 	branch = "regexp", -- This is the regexp branch, use this for the new version
+	-- 	config = function()
+	-- 		require("venv-selector").setup({
+	-- 			auto_refresh = true,
+	-- 		})
+	-- 	end,
+	-- 	keys = {
+	-- 		{ ",v", "<cmd>VenvSelect<cr>" },
+	-- 	},
+	-- },
 }
