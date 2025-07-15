3614d71622bd2291692b82ecd45cc92657444ce6
Clément Patout <2178406+clempat@users.noreply.github.com>
2025-07-15 10:14:13 +0200

    feat: add vue lsp and update

--- lua/clement/LSPs/init.lua
+++ lua/clement/LSPs/init.lua
@@ -80,6 +80,57 @@ require("lze").load({
 		for_cat = "frontend",
 		lsp = {
 			filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact", "vue" },
+			init_options = {
+				plugins = {
+					{
+						name = "@vue/typescript-plugin",
+						location = vim.fn.stdpath("data")
+							.. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+						languages = { "vue" },
+					},
+				},
+			},
+			settings = {
+				typescript = {
+					tsserver = {
+						useSyntaxServer = false,
+					},
+					inlayHints = {
+						includeInlayParameterNameHints = "all",
+						includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+						includeInlayFunctionParameterTypeHints = true,
+						includeInlayVariableTypeHints = true,
+						includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+						includeInlayPropertyDeclarationTypeHints = true,
+						includeInlayFunctionLikeReturnTypeHints = true,
+						includeInlayEnumMemberValueHints = true,
+					},
+				},
+			},
+		},
+	},
+	{
+		"volar",
+		enabled = true,
+		for_cat = "frontend",
+		lsp = {
+			filetypes = { "vue" },
+			init_options = {
+				vue = {
+					hybridMode = false, -- disables hybrid mode for better inlay hints
+				},
+			},
+			settings = {
+				typescript = {
+					inlayHints = {
+						enumMemberValues = { enabled = true },
+						functionLikeReturnTypes = { enabled = true },
+						propertyDeclarationTypes = { enabled = true },
+						parameterTypes = { enabled = true, suppressWhenArgumentMatchesName = true },
+						variableTypes = { enabled = true },
+					},
+				},
+			},
 		},
 	},
 	{
